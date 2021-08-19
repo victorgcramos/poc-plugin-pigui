a4c78f8730aa1d7eddfde3f256a0649e47789166
Victor Guedes <victorgcramos@gmail.com>
2021-08-19 19:15:20 -0300

    add politeia app

--- records/inventory.js
+++ records/inventory.js
@@ -1,10 +1,10 @@
 /**
  * Inventory is a tool responsible for dealing with record tokens
  */
-function inventoryByStatus() {
+function inventoryByStatus({ plugin = "records" }) {
   return {
     ["state"]: {
-      ["status"]: ["token"],
+      ["status"]: ["tokensArray"],
     },
   };
 }
