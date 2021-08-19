bfd810d867626dec616776dcbfa47e2dea24c2c9
Victor Guedes <victorgcramos@gmail.com>
2021-08-19 19:08:16 -0300

    initial

---
+++ records/inventory.js
@@ -0,0 +1,10 @@
+/**
+ * Inventory is a tool responsible for dealing with record tokens
+ */
+function inventoryByStatus() {
+  return {
+    ["state"]: {
+      ["status"]: ["token"],
+    },
+  };
+}
